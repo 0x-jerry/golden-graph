5101f5a40fa0a1b7361d90e2c24ad7ec38c567f3
Jerry <x.jerry.wang@gmail.com>
2026-01-14 02:42:35 +0000

    chore: build library

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts';
+import pkg from './package.json'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: ['./src/index.ts'],
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: Object.keys(pkg.dependencies)
+    }
+  },
+  plugins: [
+    vue(),
+    dts({
+      rollupTypes: true
+    }),
+  ],
+})
