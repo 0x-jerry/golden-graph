410181e6a0a1bbf865e8e295e90acd5e22ebc424
Jerry <x.jerry.wang@gmail.com>
2026-01-14 09:43:17 +0000

    chore: format code

--- src/core/dom.ts
+++ src/core/dom.ts
@@ -1,15 +1,15 @@
+import type { Optional } from '@0x-jerry/utils'
+
 export function getWorkspaceDom(workspaceId: string) {
-  return document.querySelector(`[workspace-id="${workspaceId}"]`) as
-    | HTMLElement
-    | null
-    | undefined
+  return document.querySelector(
+    `[workspace-id="${workspaceId}"]`,
+  ) as Optional<HTMLElement>
 }
 
 export function getNodeDom(workspaceId: string, nodeId: number) {
-  return getWorkspaceDom(workspaceId)?.querySelector(`[node-id="${nodeId}"]`) as
-    | HTMLElement
-    | null
-    | undefined
+  return getWorkspaceDom(workspaceId)?.querySelector(
+    `[node-id="${nodeId}"]`,
+  ) as Optional<HTMLElement>
 }
 
 export function getNodeHandleDom(
@@ -19,5 +19,5 @@ export function getNodeHandleDom(
 ) {
   return getNodeDom(workspaceId, nodeId)?.querySelector(
     `[handle-key="${handleKey}"]`,
-  ) as HTMLElement | null | undefined
+  ) as Optional<HTMLElement>
 }
